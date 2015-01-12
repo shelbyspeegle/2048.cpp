d0012b10fc8668a397af66c024628f07b19b1efa
shelbyspeegle <shelbyspeegle@gmail.com>
2015-01-12 11:58:02 -0700

    Refactor.  Move Grid logic into separate file.

--- Makefile
+++ Makefile
@@ -7,10 +7,13 @@ all: 2048
 Tile.o: Tile.cpp Tile.h
 	$(CC) $(CFLAGS) -c Tile.cpp
 
+Grid.o: Grid.cpp Grid.h
+	$(CC) $(CFLAGS) -c Grid.cpp
+
 main.o: main.cpp
 	$(CC) $(CFLAGS) -c main.cpp
 
-2048: main.o Tile.o
+2048: main.o Tile.o Grid.o
 	$(CC) $(CFLAGS) -o $@ $^ $(LFLAGS)
 
 clean:
--- main.cpp
+++ main.cpp
@@ -8,10 +8,8 @@
 
 #include <iostream>
 #include <ncurses.h>
-#include "Tile.h"
+#include "Grid.h"
 using namespace std;
-
-#define NUM_TILES 16
 
 // TODO: Handle win case.
 // TODO: See if terminal can handle colors.
@@ -19,7 +17,6 @@ using namespace std;
 // TODO: Change colors to match web version.
 
 const int START_LINE = 3;
-int score;
 int highScore = 0;
 int boardStartX;
 FILE *f;
@@ -32,211 +29,6 @@ void printGameOverMessage();
 void setup();
 void finish();
 void drawTile( Tile *tile );
-
-class Grid {
-  public:
-    Grid(){
-      clear();
-    };
-
-    void initialize() {
-      for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-          Tile t = Tile(0);
-          t.x = i;
-          t.y = j;
-          board[i][j] = t;
-        }
-      }
-    }
-
-    void clear() {
-      for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-          this->tileAt(i, j).setValue(0);
-        }
-      }
-      numOccupiedTiles = 0;
-    }
-
-    Tile tileAt(int col, int row) {
-      return this->board[col][row];
-    }
-
-    void rotateCCW() {
-      Tile tempBoard[4][4];
-
-      for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-          tempBoard[i][j] = board[i][j].copy();
-        }
-      }
-
-      for(int y = 0; y < 4; y++) {
-        for(int x = 0; x < 4; x++) {
-          board[x][y] = tempBoard[3-y][x];
-        }
-      }
-    }
-
-    void shift( int direction ) {
-      int numberOfRotationsBefore = 0;
-      int numberOfRotationsAfter = direction;
-
-      // Rotations before.
-      if (direction != 0) {
-        numberOfRotationsBefore = 4-direction;
-      }
-
-      for (int i = 0; i < numberOfRotationsBefore; i++) {
-        this->rotateCCW();
-      }
-
-      bool changeHappened = shiftRight();
-
-      // Rotations after.
-      for (int i = 0; i < numberOfRotationsAfter; i++) {
-        rotateCCW();
-      }
-
-      if (changeHappened) {
-        // Set a random free square on the board to a 2 or 4.
-        int newSquareValue = 2 * ((rand() % 2 ) + 1);
-        this->initializeFreeTile(newSquareValue);
-      }
-    }
-
-    bool shiftRight() {
-      int row;
-      int tempBoard[4][4];
-
-      for(int i = 0; i < 4; i++) {
-        for(int j = 0; j < 4; j++) {
-          tempBoard[i][j]= this->tileAt(i, j).getValue();
-        }
-      }
-
-      for ( row = 0; row < 4; row++ ) {
-        int icol = 3;
-        int jcol = 2;
-        int rowLimit = 0;
-        while (icol >= rowLimit && jcol >= rowLimit) {
-          if (this->tileAt(icol, row).getValue() == 0) {
-            icol--;
-            jcol--;
-          } else if (this->tileAt(jcol, row).getValue() == 0) {
-            jcol--;
-          } else if (this->tileAt(icol,row).getValue() == this->tileAt(jcol,row).getValue()) {
-            this->board[icol][row].setValue( this->tileAt(icol, row).getValue() * 2 );
-            score += this->tileAt(icol, row).getValue();
-            this->board[jcol][row].setValue(0);
-            icol = jcol - 1;
-            jcol = icol - 1;
-          } else {
-            icol--;
-            jcol = icol - 1;
-          }
-        }
-
-        jcol = 3;
-
-        for (icol = jcol; icol >= rowLimit; icol--) { // 0, 4, 8, 12
-          if (this->tileAt(icol, row).getValue() != 0) {
-            this->board[jcol][row].setValue( this->tileAt(icol, row).getValue() );
-            jcol--;
-          }
-        }
-
-        for (; jcol >= rowLimit; jcol--) {
-          this->board[jcol][row].setValue(0);
-        }
-      }
-
-      for (int y = 0; y < 4; y++) {
-        for (int x = 0; x < 4; x++) {
-          if ( tempBoard[x][y] != this->tileAt(x, y).getValue() ) {
-            return true; // Change happened.
-          }
-        }
-      }
-
-      return false; // No change happened.
-    }
-
-    bool checkNeighborsForMatch( int col, int row ){
-      // Check East.
-      if ( col < 3 ) {
-        if ( this->board[col][row].getValue() == this->board[col+1][row].getValue() ) {
-          return true;
-        }
-      }
-
-      // Check North.
-      if ( row > 0 ) {
-        if ( this->board[col][row].getValue() == this->board[col][row-1].getValue() ) {
-          return true;
-        }
-      }
-
-      // Check West.
-      if ( col > 1 ) {
-        if ( this->board[col][row].getValue() == this->board[col-1][row].getValue() ) {
-          return true;
-        }
-      }
-
-      // Check South.
-      if ( row < 3 ) {
-        if ( this->board[col][row].getValue() == this->board[col][row+1].getValue() ) {
-          return true;
-        }
-      }
-      return false;
-    }
-
-    bool isFull() {
-      return numOccupiedTiles == NUM_TILES;
-    }
-
-    int tilePairsExist(){
-      for ( int y = 0; y < 4; y++ ) {
-        for ( int x = 0; x < 4; x++) {
-          if (this->checkNeighborsForMatch( x, y )) {
-            return true;
-          }
-        }
-      }
-      return 0;
-    }
-
-    void initializeFreeTile( int value ){
-      Tile * t = randomFreeTile();
-
-      if (t) {
-        t->setValue(value);
-      }
-    }
-
-  private:
-    Tile board[4][4];
-    int numOccupiedTiles;
-
-    Tile * randomFreeTile(){
-      int searchCount = 0;
-      while ( true ) {
-        int randomRow = rand() % 4;
-        int randomCol = rand() % 4;
-
-        if ( this->board[randomRow][randomCol].getValue() == 0 ) {
-          return &board[randomRow][randomCol];
-        }
-
-        if (searchCount++ == NUM_TILES) {
-          return nullptr;
-        }
-      }
-    }
-};
 
 Grid playGrid;
 
@@ -355,8 +147,6 @@ void printGameOverMessage() {
 }
 
 void newGame() {
-  score = 0;
-
   playGrid.clear();
 
   playGrid.initializeFreeTile(2);
@@ -370,7 +160,7 @@ void printBoard() {
 
   boardStartX = getmaxx(stdscr)/2 - 12;
 
-  mvprintw( 0, boardStartX + 10, "Score: %6i", score);
+  mvprintw( 0, boardStartX + 10, "Score: %6i", playGrid.getScore() );
   mvprintw( 1, boardStartX + 1, "2048      Best: %6i", highScore);
 
   for (int col = 0; col < 4; col++) {
@@ -398,7 +188,7 @@ bool gameOver() {
 }
 
 void finish() {
-  highScore = score > highScore ? score : highScore;
+  highScore = playGrid.getScore() > highScore ? playGrid.getScore() : highScore;
 
   f = fopen(".scores", "w");
   fprintf(f, "%i", highScore);
